2327f09cf8c5cc5a78692e2c4731d1a66646feca
Bartosz Kosakowski <kosakowb@mcmaster.ca>
2017-03-23 14:35:15 -0400

    removed tests from sort.java, properly commented code

--- nosophobic-dev/src/ndev/Sort.java
+++ nosophobic-dev/src/ndev/Sort.java
@@ -5,32 +5,7 @@ package ndev;
  * disease, and the frequency of the disease using quicksort.
  * @author Bartosz Kosakowski 
  */
-public class Sort {
-	//############## USED FOR TESTING
-	//int year, String state, String topic, String dataType, float valueAv, float valueHc, float valueLc, float lo, float la
-	public static void main (String [] args){
-		CDI test1 = new CDI(10, "Bartoszland", "Aids", "wat", 7, 6, 7, 6, 7);
-		CDI test2 = new CDI(10, "California", "Cancer", "wat", 7, 6, 7, 6, 7);
-		CDI test3 = new CDI(10, "Illinois", "Alcoholism", "wat", 7, 6, 7, 6, 7);
-		CDI test4 = new CDI(10, "Georgia", "Syphillis", "wat", 7, 6, 7, 6, 7);
-		CDI test5 = new CDI(10, "Texas", "Gangern", "wat", 7, 6, 7, 6, 7);
-		
-		CDI[] sortTest = {test3, test5, test1, test4, test2};
-		
-		System.out.println("state:");
-		Sort.toString(sortTest);
-		System.out.println("============================");
-		Sort.sort(sortTest, "state");
-		Sort.toString(sortTest);
-		System.out.println("\n");
-		
-		System.out.println("disease:");
-		Sort.toString(sortTest);
-		System.out.println("============================");
-		Sort.sort(sortTest, "disease");
-		Sort.toString(sortTest);
-	}//end of main
-	
+public class Sort {	
 	/**
 	 * Main sorting function, calls all other functions required to perfomr a quicksort.
 	 * @param data - a CDI array to be sorted.
@@ -40,6 +15,13 @@ public class Sort {
 		sort(data, 0, data.length - 1, sortBy);
 	}
 
+	/**
+	 * Used to sort the data by whatever is specified by sortBy
+	 * @param data - CDI array to be sorted
+	 * @param lo - int value representing the left-most value of the array
+	 * @param hi - int value representing the right-most value of the array
+	 * @param sortBy - String representing how the data should be sorted (disease, state, danger (default sort type))
+	 */
 	private static void sort(CDI[] data, int lo, int hi, String sortBy) {
 		if (hi <= lo)
 			return;
@@ -48,6 +30,14 @@ public class Sort {
 		sort(data, j + 1, hi, sortBy); // Sort right part data[j+1 .. hi].
 	}
 
+	/**
+	 * Performs a partition of a given array
+	 * @param data - CDI array to be partitioned
+	 * @param lo - low end of the array
+	 * @param hi - high end of the array
+	 * @param sortBy - String representing how the data should be sorted (disease, state, danger (default sort type))
+	 * @return int representing the new pivot value to use in further partitions
+	 */
 	private static int partition(CDI[] data, int lo, int hi, String sortBy) { 
 		int i = lo, j = hi + 1; // left datand right scan indices
 		CDI v = data[lo]; // partitioning item
@@ -72,7 +62,7 @@ public class Sort {
 	 * If sortBy is not "disease" or "state", it automatically sorts by danger
 	 * @param current - the current CDI being compared.
 	 * @param next - the next CDI being compared.
-	 * @param sortBy - the search criteria do be sorted by.
+	 * @param sortBy - String representing how the data should be sorted (disease, state, danger (default sort type))
 	 * @return boolean value indicating if the value of current is less than the value of next (true if so, else false).
 	 */
 	private static boolean lessBy(CDI current, CDI next, String sortBy) {
@@ -83,13 +73,23 @@ public class Sort {
 		else 
 			return (current.getDanger() < next.getDanger());
 	}// end of lessBy method
-
+	
+	/**
+	 * Performs a swap on the values in the array
+	 * @param data - CDI array to be sorted
+	 * @param i - index of first element to be swapped
+	 * @param j - index of second element to be swapped
+	 */
 	private static void swap(CDI[] data, int i, int j) {
 		CDI t = data[i];
 		data[i] = data[j];
 		data[j] = t;
 	}// end of swap method
 	
+	/**
+	 * Prints the data to the console
+	 * @param data - CDI array to be printed
+	 */
 	public static void toString(CDI[] data){
 		for (int i = 0; i < data.length; i++)
 			System.out.println(data[i].getTopic() + " | " + data[i].getState());
