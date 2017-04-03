b8f1175645a634093f21dc402cf8fb03ce9c4c3b
yukseltron <h.yuksel04@gmail.com>
2017-04-03 12:54:38 -0400

    implemented google map api

--- website/main.js
+++ website/main.js
@@ -1,18 +1,8 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
+function initMap() {
+  var usa = {lat: 39.8282, lng: -98.5795};
 
-function filterFunction() {
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    div = document.getElementById("myDropdown");
-    a = div.getElementsByTagName("a");
-    for (i = 0; i < a.length; i++) {
-        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-            a[i].style.display = "";
-        } else {
-            a[i].style.display = "none";
-        }
-    }
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: usa
+  });
 }
