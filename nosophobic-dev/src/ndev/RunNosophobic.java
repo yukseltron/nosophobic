e643c7d7c3a35c4f8fe74a31b2faf524a9ebb8fd
yukseltron <h.yuksel04@gmail.com>
2017-04-12 18:04:28 -0400

    updates to runNosophobic

--- nosophobic-dev/src/ndev/RunNosophobic.java
+++ nosophobic-dev/src/ndev/RunNosophobic.java
@@ -2,7 +2,10 @@ package ndev;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
+
+import com.sun.corba.se.impl.orbutil.graph.Graph;
 
 /**
  * Puts all of the modules together and runs the project.
@@ -14,11 +17,13 @@ public class RunNosophobic {
 	public static void main(String[] args) throws Exception {
 		DataCollection.readFile();
 		Scanner userIn = new Scanner(System.in);
-		System.out.println("Enter either \"state\", or \"both\" for state and disease:");
+		System.out.println("Enter either \"state\", \"disease\", or \"both\":");
 		String userSelection = userIn.nextLine();
 
 		if (userSelection.toLowerCase().equals("both")) {
-			doBoth();
+			System.out.println("Enter either info or graph");
+			userSelection = userIn.nextLine();
+			doBoth(userSelection);
 		}
 		/**
 		 * if the user enters just by state, then the output will be the CDIs
@@ -26,30 +31,56 @@ public class RunNosophobic {
 		 */
 		else if (userSelection.toLowerCase().equals("state")) {
 			doState();
-		} // end of "state" branch
+		}
+		else if (userSelection.toLowerCase().equals("disease")) {
+			doDisease();
+		}
 		userIn.close();
 	}// end of main method
 
-	private static void doBoth() throws Exception {
+	private static void doBoth(String userSelection) throws Exception {
 		Scanner userIn = new Scanner(System.in);
 		System.out.println("Enter a US state abbreviation: ");
 		String stateChosen = userIn.nextLine();
 		System.out.println("Enter a disease: ");
 		String diseaseChosen = userIn.nextLine();
-
-		ArrayList<CDI> listNodes = Filter.filterDisease(Filter.filterState(DataCollection.getList(), stateChosen), diseaseChosen);
-		CDI[] arrayNodes = new CDI[listNodes.size()];
 		
-		for (int i = 0; i < arrayNodes.length; i++) {
-			arrayNodes[i] = listNodes.get(i);
+		if (userSelection.equals("info")){
+			ArrayList<CDI> listNodes;
+			CDI[] arrayNodes;
+			listNodes = Filter.filterDisease(Filter.filterState(DataCollection.getList(), stateChosen), diseaseChosen);
+			arrayNodes = new CDI[listNodes.size()];
+			
+			for (int i = 0; i < arrayNodes.length; i++) {
+				arrayNodes[i] = listNodes.get(i);
+			}
+
+			Sort.sort(arrayNodes, "danger");
+			System.out.println("Top 10 CID's for " + diseaseChosen + " in " + stateChosen);
+			
+			for (int i = arrayNodes.length - 1; i > arrayNodes.length - 11; i--) {
+				System.out.printf("Danger: %f	Longitude: %f	Latitude: %f\n", arrayNodes[i].getDanger(),
+						arrayNodes[i].getLo(), arrayNodes[i].getLa());
+			}
 		}
-
-		Sort.sort(arrayNodes, "danger");
-		System.out.println("Top 10 CID's for " + diseaseChosen + " in " + stateChosen);
 		
-		for (int i = arrayNodes.length - 1; i > arrayNodes.length - 11; i--) {
-			System.out.printf("Danger: %f	Longitude: %f	Latitude: %f\n", arrayNodes[i].getDanger(),
-					arrayNodes[i].getLo(), arrayNodes[i].getLa());
+		else if (userSelection.equals("graph")){
+			System.out.println("Determaining safest path");
+			ArrayList<String> path = new ArrayList<String>();
+			Graph america = new Graph();
+			String state = stateChosen;
+			path.add(state);
+			
+			while (betterEdge(state, diseaseChosen, america, DataCollection.getList()) != state){
+				state = betterEdge(state, diseaseChosen, america, DataCollection.getList());
+				path.add(state);
+			}
+			
+			System.out.println("Outputting path");
+			
+			for (int i = 0; i < path.size(); i++)
+				System.out.println(path.get(i));
+			
 		}
 		userIn.close();
 	}
@@ -102,6 +133,41 @@ public class RunNosophobic {
 		userIn.close();
 	}
 	
+	private static void doDisease() throws Exception {
+		Scanner userIn = new Scanner(System.in);
+		ArrayList<CDI> sameDiseaseCDI;
+		System.out.println("Enter a disease: ");
+		String diseaseChosen = userIn.nextLine();
+
+		// Filters out the data that isn't associated with the given disease
+		sameDiseaseCDI = Filter.filterDisease(DataCollection.getList(), diseaseChosen);
+
+		CDI[] sameDiseaseCDIArray = new CDI[sameDiseaseCDI.size()];
+		sameDiseaseCDIArray = sameDiseaseCDI.toArray(sameDiseaseCDIArray);
+		Sort.sort(sameDiseaseCDIArray, "state");
+		ArrayList<CDI> sortedList = new ArrayList<CDI>(Arrays.asList(sameDiseaseCDIArray));
+		int totalStates = 0;
+
+		// Stores the states and the danger level in a HashMap
+		HashMap<String, String> map = new HashMap<String, String>();
+		for (int i = 0; i < sameDiseaseCDIArray.length - 1; i++){
+			if (sameDiseaseCDIArray[i].getState() != sameDiseaseCDIArray[i+1].getState()){
+				float dangerLevel = sumDanger(sameDiseaseCDI, sameDiseaseCDIArray[i].getState(), diseaseChosen);
+				map.put(sameDiseaseCDIArray[i].getState(), String.valueOf(dangerLevel));
+			}
+		}
+		
+		System.out.println("\nThe sum of the CDI's pertaining to the disease " + diseaseChosen + " for each state are: ");
+		System.out.println("(State, Danger level)");
+		
+		for (String name: map.keySet()){
+	        String key =name.toString();
+	        String value = map.get(name).toString();
+			System.out.println(key + " " + value);
+		}
+		userIn.close();
+	}
+	
 	/**
 	 * Sums up the danger values for all of the diseases in a given state and stores them
 	 * in outputData. sortedData is needed for sumDanger to compute the sum of the danger
@@ -134,6 +200,26 @@ public class RunNosophobic {
 			sumDanger += c.getDanger();
 		}
 		return sumDanger;
-	}// end of sumDanger
+	}
 
-}// end of RunNosophobic
+	public static String betterEdge(String state, String disease, Graph graph, ArrayList<CDI> cdis){
+		float currentVal, cmpVal, minVal;
+		String minState;
+		try{
+			currentVal = sumDanger(cdis, state, disease);
+			minState = state;
+			minVal = currentVal;
+			for (String contigState : graph.getEdges(state)){
+				cmpVal = sumDanger(cdis, contigState, disease);
+				if (cmpVal < minVal){
+					minVal = cmpVal;
+					minState = contigState;
+				}
+			}
+			return minState;
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		return state;
+	}
+}
